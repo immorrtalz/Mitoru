8c3699e12de162c376436e44466e8a07e63d35d4
immorrtalz <61843974+immorrtalz@users.noreply.github.com>
2026-07-06 21:44:51 +0500

    first deploy

--- src/context/SettingsContext.ts
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-import { SettingsContextValue, initialSettingsContextValue } from '../misc/settings';
-
-const SettingsContext = createContext<SettingsContextValue>(initialSettingsContextValue);
-
-export default SettingsContext;
--- src/hooks/useDialog.ts
+++
@@ -1,9 +0,0 @@
-import { useContext } from "react";
-import { DialogContext } from "../context/DialogContext";
-
-export default function useDialog()
-{
-	const ctx = useContext(DialogContext);
-	if (!ctx) throw new Error("useDialog must be used within <DialogProvider>");
-	return ctx;
-}
